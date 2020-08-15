2680db554480d8e590f99002b646b795f8d116c9
aafinger <aafinger@iastate.edu>
2020-08-14 20:40:32 -0500

    fixed bug, removed _builds

---
+++ cmake_private_depend2.cpp
@@ -0,0 +1,7 @@
+#include "cmake_private.hpp"
+#include "cmake_private_depend.hpp"
+#include "cmake_private_depend2.hpp"
+
+int call_cmake_private_depend2(){
+  return call_cmake_private_depend()+2;
+}
---
+++ cmake_private_depend2.hpp
@@ -0,0 +1,6 @@
+#ifndef CMAKE_PRIVATE_DEPEND2_HPP
+#define CMAKE_PRIVATE_DEPEND2_HPP
+
+int call_cmake_private_depend2();
+
+#endif
